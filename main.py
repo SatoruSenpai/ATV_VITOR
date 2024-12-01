960e1bc395b03914fe46c1cdc6265979b9164d8b
Victor Luiz Nery <164579575+SatoruSenpai@users.noreply.github.com>
2024-12-01 17:39:24 -0300

    V1.0.3

--- main.py
+++ main.py
@@ -7,8 +7,18 @@ app_vitor = Flask (__name__)
 def raiz():
     return 'Olá, turma!'
 
-# fea código aula 02
+# Fea código aula 02
 def saudacoes(nome):
     return f'Olá, {nome}'
+
+# Fea código aula 03
+@app_vitor.route('/rota1')
+def rota1():
+    return 'Olá Usuáriooo!!'
+@app_vitor.route('/rota2')
+def rota2():
+    resposta = "<h3> Essa é uma página da rota 2 </h3>"
+    return resposta
+# Fea código aula 02
 if __name__ == "_main_":
-  app_vitor.run()
+    app_vitor.run()
