341d759e6628e9e4dd924420cddc9d3750891002
Victor Luiz Nery <164579575+SatoruSenpai@users.noreply.github.com>
2024-12-01 17:45:39 -0300

    V1.0.4

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
-# Código da aula 01
+# Código da aula 01 eeee aula 04
 from flask import Flask
 
 app_vitor = Flask (__name__)
-
-@app_vitor.route('/')
-def raiz():
-    return 'Olá, turma!'
-
-# Fea código aula 02
+# Feature código aula 02
 def saudacoes(nome):
     return f'Olá, {nome}'
 
-# Fea código aula 03
+# Feature código aula 03
 @app_vitor.route('/rota1')
 def rota1():
-    return 'Olá Usuáriooo!!'
+    return 'Olá Usuário!!'
+
 @app_vitor.route('/rota2')
 def rota2():
     resposta = "<h3> Essa é uma página da rota 2 </h3>"
     return resposta
-# Fea código aula 02
+
+# Feature código aula 04
+@app_vitor.route("/")
+def homepage():
+    return("homepage.html")
+@app_vitor.route("/contato")
+def contato():
+    return("contato.html")
+# Feature código aula 02
 if __name__ == "_main_":
     app_vitor.run()
